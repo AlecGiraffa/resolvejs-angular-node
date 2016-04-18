65efbce7c44c0656cd9cda1954242bd95c61ceff
Minhui <zhmh1025@hotmail.com>
2016-04-17 23:54:09 -0700

    Bootstap project with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name resolvejsAngularNodeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the resolvejsAngularNodeApp
+ */
+angular.module('resolvejsAngularNodeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
